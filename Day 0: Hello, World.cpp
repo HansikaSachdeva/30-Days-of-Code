c162252802bd5d9b71acbd5d0114cd68510bb8e3
Hansika Sachdeva <52819652+HansikaSachdeva@users.noreply.github.com>
2021-07-01 19:13:14 +0530

    Update Day 0: Hello, World.cpp

--- Day 0: Hello, World.cpp
+++ Day 0: Hello, World.cpp
@@ -1,7 +1,13 @@
 int main()
 {
+    //declare string variable to hold the input
     string str;
+    
+    //get a line of input from cin and save it to the variabe str
     getline(cin, str);
+    
+    //output using cout
     cout<<"Hello, World."<<endl<<str;
+    
     return 0;
 }
