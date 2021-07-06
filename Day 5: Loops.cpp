528e5eda2e9535093613458cd5dd7c00c61ebd90
Hansika Sachdeva <52819652+HansikaSachdeva@users.noreply.github.com>
2021-07-06 15:26:08 +0530

    Create Day 5: Loops.cpp

---
+++ Day 5: Loops.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //declaring variables
+    int i, n;
+  
+    //taking input
+    cin>>n;
+  
+    //running a loop to print the result
+    for(i=1; i<=10; i++){
+        cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+    }
+}
